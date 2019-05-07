34b838cc33b6f4e370902949ba2d08930ff5ef4c
Subin Kim <s2017s24@e-mirim.hs.kr>
2019-05-07 11:51:20 +0900

    #2 사용자 입력을 출력하도록 구현

--- StringIO_TextView/Form1.Designer.cs
+++ StringIO_TextView/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.txtEdit = new System.Windows.Forms.TextBox();
             this.btn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtEdit
@@ -42,33 +42,36 @@
             // 
             // btn
             // 
+            this.btn.Font = new System.Drawing.Font("굴림", 10F);
             this.btn.Location = new System.Drawing.Point(13, 40);
             this.btn.Name = "btn";
             this.btn.Size = new System.Drawing.Size(311, 91);
             this.btn.TabIndex = 1;
             this.btn.Text = "입력";
             this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
             // 
-            // label1
+            // lblResult
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 14F);
-            this.label1.Location = new System.Drawing.Point(13, 138);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 19);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "결과 : ";
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("굴림", 14F);
+            this.lblResult.Location = new System.Drawing.Point(13, 138);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(65, 19);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "결과 : ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(336, 211);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
             this.Controls.Add(this.btn);
             this.Controls.Add(this.txtEdit);
             this.Name = "Form1";
             this.Text = "문자 입출력";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,7 +81,7 @@
 
         private System.Windows.Forms.TextBox txtEdit;
         private System.Windows.Forms.Button btn;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- StringIO_TextView/Form1.cs
+++ StringIO_TextView/Form1.cs
@@ -11,9 +11,20 @@ namespace StringIO_TextView
 {
     public partial class Form1 : Form
     {
+        string OrgStr = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OrgStr = this.lblResult.Text;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.txtEdit.Text;
         }
     }
 }
